97f9d778e95d075f194cbbc1cead7408c4d9d44c
Sérgio Rafael Siqueira <sergiorsiqueira9@gmail.com>
2015-07-03 02:26:07 -0300

    first commit

---
+++ event.js
@@ -0,0 +1,21 @@
+(function() {
+  window.HTMLElement = (typeof HTMLElement === 'undefined' && Element) || HTMLElement;
+
+  HTMLElement.prototype.on = function(type, listener, useCapture) {
+    useCapture = useCapture || false;
+
+    if (this.attachEvent) {
+      return this.attachEvent('on' + type, listener);
+    }
+    return this.addEventListener(type, listener, useCapture);
+  }
+
+  HTMLElement.prototype.off = function(type, listener, useCapture) {
+    useCapture = useCapture || false;
+
+    if (this.detachEvent) {
+      return this.detachEvent('on' + type, listener);
+    }
+    return this.removeEventListener(type, listener, useCapture);
+  }
+})();
