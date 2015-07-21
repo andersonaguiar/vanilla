05ba799d8d9593b0dc72e8c990cbf403a467ecc9
Anderson Aguiar <revsoft10@gmail.com>
2015-07-21 13:39:51 -0300

    Update remove.js

--- remove.js
+++ remove.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  window.HTMLElement = (typeof HTMLElement === 'indefined' && Element) || HTMLElement;
+  window.HTMLElement = (typeof HTMLElement === 'undefined' && Element) || HTMLElement;
 
   HTMLElement.prototype.remove = function() {
     this.parentNode.removeChild(this);
